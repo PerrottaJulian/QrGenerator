051cf746d4a88afcc4655b79b4fd055004f9285b
PerrottaJulian <111912746+PerrottaJulian@users.noreply.github.com>
2024-10-23 20:30:25 -0300

    Initial Commit

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 import qrcode
 
+#Ingresar URL de donde se quiere que le QR redireccione
 url = input("Ingrese una URL: ")
 
+#Se crea el objeto QR
 qr = qrcode.QRCode(
     version = 1,
     box_size=10,
     border=2.5
 )
 
+#Añado la URL al objeto QR
 qr.add_data(url)
 qr.make(fit=True)
 
+#Creo la imagen
 img = qr.make_image()
 img.save("TuQR.png")
